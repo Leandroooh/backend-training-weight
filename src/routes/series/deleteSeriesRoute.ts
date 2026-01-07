223ba3eee4c394a06de7ef91fe18405ccd3c3878
leeandrooh <ls@dev.com>
2026-01-07 15:11:32 -0300

    feat: features to deploy

--- src/routes/series/deleteSeriesRoute.ts
+++ src/routes/series/deleteSeriesRoute.ts
@@ -4,34 +4,39 @@ import z from "zod";
 import { AuthHandler } from "@/middlewares/AuthHandler.js";
 import { prisma } from "@/prisma/client.js";
 
-export async function GetSeriesRoute(app: FastifyInstance) {
+export async function deleteSeries(app: FastifyInstance) {
 	app
 		.withTypeProvider<ZodTypeProvider>()
 		.register(AuthHandler)
-		.get(
-			"/exercise/:exerciseEntryId/series",
+		.delete(
+			"/series/:id",
 			{
 				schema: {
 					tags: ["Series"],
 					params: z.object({
-						exerciseEntryId: z.string(),
+						id: z.string(),
 					}),
 				},
 			},
 			async (request, reply) => {
-				const { exerciseEntryId } = request.params;
-				const series = await prisma.exerciseSeries.findMany({
+				const { id } = request.params;
+
+				const series = await prisma.exerciseSeries.findFirst({
 					where: {
-						exerciseEntryId,
-					},
-					select: {
-						id: true,
-						reps: true,
-						set: true,
-						seriesWeight: true,
+						id,
 					},
 				});
-				return reply.status(200).send(series);
+				if (!series) {
+					return reply
+						.status(404)
+						.send({ message: "Series not found" });
+				}
+				await prisma.exerciseSeries.delete({
+					where: {
+						id,
+					},
+				});
+				return reply.status(200).send({ message: "Series deleted" });
 			},
 		);
 }
